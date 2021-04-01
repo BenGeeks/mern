c3e1736446faeaa8f2bdfb1dd817ec8f598c8cfc
Ben Gican <b3nhur77@gmail.com>
2021-04-01 11:17:52 +0800

    Fix dotenv security issue

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/post.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -14,10 +16,8 @@ app.use(cors());
 app.use('/post', postRoutes);
 // Mongo Database Atlas
 
-const DBCONNECTION =
-  process.env.DBCONNECTION ||
-  'mongodb+srv://mymernboilerplate:mymernboilerplate0517@cluster0.ere55.mongodb.net/mern?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const DBCONNECTION = process.env.DBCONNECTION;
+const PORT = process.env.PORT;
 
 mongoose
   .connect(DBCONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
